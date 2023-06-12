dc4b4a7ddf4582cf35b9a0584bf141c436e67510
Krishna.P.G <110535341+Krishna-P-G@users.noreply.github.com>
2023-06-12 19:15:02 +0530

    Delete Bodhi-Plus-Backend/src directory

--- Bodhi-Plus-Backend/src/main/java/com/example/Bodhi/Plus/Repos/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Bodhi.Plus.Repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Bodhi.Plus.Model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long>{
-	
-}
